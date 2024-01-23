7538bb24a66b716a8abc30eac69662ba33dc38c0
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2024-01-22 22:48:41 -0600

    Adding new components. Adding more event db actins. Modified EventDetails and Home page.

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -18,6 +18,21 @@ import {
 } from '@/types'
 
 /**
+ * The function `populateEvent` populates the `organizer` and `category` fields of a query with
+ * additional information from the `User` and `Category` models.
+ * @param {any} query - The `query` parameter is an object that represents a query to be executed on a
+ * database. It is used to populate the fields of an event object with additional information from
+ * related models.
+ * @returns The function `populateEvent` is returning the result of the `query.populate` method, which
+ * is a modified query object with the specified population options.
+ */
+const populateEvent = (query: any) => {
+    return query
+        .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+        .populate({ path: 'category', model: Category, select: '_id name' })
+}
+
+/**
  * The function creates a new event, associates it with an organizer, and updates a specified path.
  * @param {CreateEventParams}  - - `userId`: The ID of the user creating the event.
  * @returns the newly created event as a JSON object.
@@ -35,6 +50,66 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
         revalidatePath(path)
 
         return JSON.parse(JSON.stringify(newEvent))
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+/**
+ * The function `getEventById` retrieves an event from a database by its ID and returns it as a JSON
+ * object.
+ * @param {string} eventId - The `eventId` parameter is a string that represents the unique identifier
+ * of the event that we want to retrieve.
+ * @returns a Promise that resolves to the event object with the specified eventId.
+ */
+export async function getEventById(eventId: string) {
+    try {
+        await connectToDatabase()
+
+        const event = await populateEvent(Event.findById(eventId))
+
+        if (!event) throw new Error('Event not found')
+
+        return JSON.parse(JSON.stringify(event))
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+export async function getAllEvents({ query, limit = 6, page, category }: GetAllEventsParams) {
+    try {
+        await connectToDatabase()
+
+        const conditions = {}
+
+        const eventsQuery = Event.find(conditions)
+            .sort({ createdAt: 'desc' })
+            .skip(0)
+            .limit(limit)
+
+        const events = await populateEvent(eventsQuery)
+        const eventsCount = await Event.countDocuments(conditions)
+
+        return {
+            data: JSON.parse(JSON.stringify(events)),
+            totalPages: Math.ceil(eventsCount / limit)
+        }
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+/**
+ * The function deletes an event from the database and revalidates a given path if the event was
+ * successfully deleted.
+ * @param {DeleteEventParams}  - - `eventId`: The ID of the event to be deleted.
+ */
+export async function deleteEvent({ eventId, path }: DeleteEventParams) {
+    try {
+        await connectToDatabase()
+
+        const deletedEvent = await Event.findByIdAndDelete(eventId)
+        if (deletedEvent) revalidatePath(path)
     } catch (error) {
         handleError(error)
     }
